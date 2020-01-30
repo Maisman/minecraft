04166c888aa6140d4befb3608e4e8236f70b6761
Maisman <60487166+Maisman@users.noreply.github.com>
2020-01-30 21:52:00 +0100

    Added Blau

---
+++ src/main/java/net/mcreator/dimensional_dungeon/ServerProxydimensional_dungeon.java
@@ -0,0 +1,24 @@
+package net.mcreator.dimensional_dungeon;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class ServerProxydimensional_dungeon implements IProxydimensional_dungeon {
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
